0e105bd7334a3d3528081c126a917191eb218325
Jorge Bastida <me@jorgebastida.com>
2017-06-06 09:44:15 +0100

    Fix python3 tests

--- aws2fa/core.py
+++ aws2fa/core.py
@@ -100,7 +100,7 @@ class AWS2FA(object):
         if 'source_profile' not in configuration:
             parser.set(self._config_profile_name(), 'source_profile', self._get_source_profile_name())
 
-        with open(self._config_path, 'wb') as f:
+        with open(self._config_path, 'w') as f:
             parser.write(f)
         return serial_number
 
@@ -129,7 +129,7 @@ class AWS2FA(object):
             credentials_parser.add_section(self._get_source_profile_name())
             credentials_parser.set(self._get_source_profile_name(), 'aws_secret_access_key', profile_credentials['aws_secret_access_key'])
             credentials_parser.set(self._get_source_profile_name(), 'aws_access_key_id', profile_credentials['aws_access_key_id'])
-            with open(self._credentials_path, 'wb') as f:
+            with open(self._credentials_path, 'w') as f:
                 credentials_parser.write(f)
 
     def _set_session_credentials(self, **kwargs):
@@ -139,7 +139,7 @@ class AWS2FA(object):
         for k, v in kwargs.items():
             parser.set(self.profile, k, v)
 
-        with open(self._credentials_path, 'wb') as f:
+        with open(self._credentials_path, 'w') as f:
             parser.write(f)
 
         return parser
